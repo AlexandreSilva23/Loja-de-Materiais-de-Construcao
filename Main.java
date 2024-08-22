cc825666161cd97ce2981cd91c965eb55c0752b8
AlexandreSilva23 <alexandresilva.ch026@academico.ifsul.edu.br>
2024-08-22 10:32:44 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,15 +21,13 @@ public class Main {
         adicionarProduto(new Produto.Materiais(17.90, "58V27LB", new Date(), 150, "Brita 1", 100.0, 0.0, 0.0, 0.0));
         adicionarProduto(new Produto.Materiais(45.00, "58V28LB", new Date(), 75, "Cal Hidratada", 40.0, 25.0, 15.0, 10.0));
         adicionarProduto(new Produto.Materiais(30.50, "58V29LB", new Date(), 50, "Tijolo 8 Furos", 20.0, 20.0, 20.0, 10.0));
-
-        // Aqui adiciono as tintas:        
+      
         adicionarProduto(new Produto.Tintas(150.00, "59A01LB", new Date(), 30, "Branca", 18.0, "Fosco"));
         adicionarProduto(new Produto.Tintas(160.00, "59A02LB", new Date(), 25, "Azul", 18.0, "Brilhante"));
         adicionarProduto(new Produto.Tintas(155.00, "59A03LB", new Date(), 20, "Verde", 18.0, "Acetinado"));
         adicionarProduto(new Produto.Tintas(145.00, "59A04LB", new Date(), 35, "Amarela", 18.0, "Fosco"));
         adicionarProduto(new Produto.Tintas(170.00, "59A05LB", new Date(), 15, "Vermelha", 18.0, "Brilhante"));
 
-        // E por fim as ferramentas: 
         adicionarProduto(new Produto.Ferramentas(250.00, "60B01LB", new Date(), 15, "Martelete", "Bosch"));
         adicionarProduto(new Produto.Ferramentas(300.00, "60B02LB", new Date(), 10, "Serra Circular", "DeWalt"));
         adicionarProduto(new Produto.Ferramentas(350.00, "60B03LB", new Date(), 8, "Parafusadeira", "Makita"));
